af803e07a7f63a8c4958376c1af49718a9e3ebdf
florian <florianbarjon@icloud.com>
2025-03-06 18:18:08 +0100

    redux

--- src/actions.js
+++ src/actions.js
@@ -1,6 +1,7 @@
 export const ADD_EMPLOYEE = "ADD_EMPLOYEE";
 export const REMOVE_EMPLOYEE = "REMOVE_EMPLOYEE";
 export const ARCHIVE_EMPLOYEE = "ARCHIVE_EMPLOYEE";
+export const LOAD_EMPLOYEES = "LOAD_EMPLOYEES"; // Ajoutez cette ligne
 
 export const addEmployee = (employee) => ({
   type: ADD_EMPLOYEE,
@@ -15,4 +16,10 @@ export const removeEmployee = (employeeId) => ({
 export const archiveEmployee = (employeeId) => ({
   type: ARCHIVE_EMPLOYEE,
   payload: employeeId,
+});
+
+// Ajoutez cette action
+export const loadEmployees = (employees) => ({
+  type: LOAD_EMPLOYEES,
+  payload: employees,
 });
